c2ba6fb7b8695bcc09d1fd0025c45ae21871bbb9
nataliestam <natalielynnstam@gmail.com>
2018-07-12 11:06:01 -0700

    first commit

---
+++ client/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default App;
